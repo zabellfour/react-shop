a6b38f71431db4244788c5bb0bd3ff54afc9a887
Tatyana Strelchenko <tatyana.strelchenko@rgand.com>
2018-05-30 20:49:00 +0300

    add card structure

--- src/Basket/index.js
+++ src/Basket/index.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default class Basket extends React.Component{
     render(){
-        return <div className="basket">Basket component</div>
+
+    	
+        return (
+			<div className="basket">
+				Basket component
+				<ul className="good-list">
+					<li></li>
+				</ul>
+			</div>
+        )
     }
 }
--- src/Goods/GoodsCard/index.js
+++ src/Goods/GoodsCard/index.js
@@ -1,9 +1,42 @@
 import React from "react";
 
 export default class GoodsCard extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    addGoods(){
+        //TODO
+
+        console.log(this.props.card);
+
+        const card = localStorage.getItem('key');
+        const cardParse = JSON.parse(card) || {};
+
+        console.log(cardParse);
+
+        for(let index in cardParse) {
+            if(Object.keys(cardParse).includes(this.props.card['product-id'])) {
+                cardParse[index]['product-count'] += 1;
+                return;
+            }
+            cardParse[index]['product-count'] = 1;
+        }
+
+        if(!Object.keys(cardParse).length) {
+
+            cardParse[this.props.card['product-id']] = {};
+            cardParse[this.props.card['product-id']]['product-count'] = 1;
+        }
+
+        localStorage.setItem('key', JSON.stringify(cardParse));
+
+    }
+
     render(){
         
-        let cardData = this.props.card ;
+        let cardData = this.props.card;
+
         return (
             <div  className="card">
                 <div className="img-holder">
@@ -15,8 +48,13 @@ export default class GoodsCard extends React.Component{
                 </div>
                 <h3>{cardData['product-name']}</h3>
                 <p>{cardData['product-description']}</p>
-                <a href={cardData['product-url']}>See more</a>
-                
+                <div className="links-holder">
+                    <a href={cardData['product-url']}>See more</a>
+                    <span 
+                        className="add-to-card"
+                        onClick={this.addGoods.bind(this)}>Add to card
+                    </span>
+                </div>
             </div>
         )
     }
