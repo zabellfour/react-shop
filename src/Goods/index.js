266fe11f5c623ba6f9a8a735e9fd4c4e24701aa7
Alit4inov <alit4inov@gmail.com>
2018-06-01 00:14:02 +0200

    added livesearch

--- src/Goods/index.js
+++ src/Goods/index.js
@@ -2,11 +2,32 @@ import React from "react";
 import GoodsCard from "./GoodsCard"
 export default class Goods extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+          value : ''
+        };
+        this.setValue = e => {
+          const inputedValue = e.target.value ;
+          this.setState({value : inputedValue});
+        }
+        this.filter = el => {
+          return el['product-name'].toUpperCase().includes(this.state.value.toUpperCase())
+        }       
+      }
+
     render(){
        
         return <div  className="goods">
+
+            <div class="search-field">
+              <input value={this.state.value} onChange={this.setValue} placeholder="type to search"/>
+            </div>
+            
             {
-                this.props.items.map((el, i ) => <GoodsCard key = {i} card = {el}/>)
+                
+                this.props.items.filter(this.filter).map((el, i ) => <GoodsCard key = {i} card = {el}/>)
+
             }
         </div>
     }
